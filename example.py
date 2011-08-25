c3a50e7db761f732243413283bd38c38b7696dc9
Fábio Cerqueira <fabio.cerqueira@mobtelecom.com.br>
2011-08-25 17:40:37 -0300

    example modify

--- example.py
+++ example.py
@@ -2,7 +2,6 @@ import nbio
 print nbio.init()
 print nbio.open()
 print nbio.get_info()
-print nbio.set_info(50,3)
-print nbio.get_info()
 print nbio.enroll('fabio')
+print nbio.verify()
 print nbio.close()
