a6cb778c7e851d16aedfa4f784eba43ce6542581
Fábio Cerqueira <cerqueirasfabio@gmail.com>
2011-08-30 12:39:04 -0300

    fix pep8 style

--- complete_example.py
+++ complete_example.py
@@ -4,9 +4,11 @@ import sys
 
 import nbio
 
+
 class User(object):
     seq = 0
     objects = []
+
     def __init__(self, name, digital=''):
         self.name = name
         self.digital = digital
@@ -19,7 +21,7 @@ class User(object):
                 out.append(u)
         return out
 
-    @classmethod 
+    @classmethod
     def find_by_digital(cls, verify):
         for u in cls.objects:
             if verify(u.digital):
@@ -27,7 +29,9 @@ class User(object):
 
     def save(self):
         usr = User.find_by_name(self.name)
-        usr_digital = User.find_by_digital(lambda x: nbio.verify_match(self.digital,x))
+        usr_digital = User.find_by_digital(
+                            lambda x: nbio.verify_match(self.digital, x)
+                        )
         if usr or usr_digital:
             raise ValueError('Este usuário já está cadastrado.')
         User.seq += 1
@@ -39,6 +43,7 @@ class User(object):
 
     def __str__(self):
         return self.name
+
 
 def cadastrar():
     name = raw_input('Digite o nome do usuário: ')
@@ -52,18 +57,22 @@ def cadastrar():
     else:
         print 'Usuário %s cadastrado com sucesso.' % new_user
 
+
 def verificar():
     print 'Posicione o dedo no leitor biométrico'
     digital = nbio.enroll()
-    user = User.find_by_digital(lambda x:nbio.verify_match(digital, x))
+    user = User.find_by_digital(lambda x: nbio.verify_match(digital, x))
     if user:
         print 'Usuário encontrado:', user
     else:
         print 'Nenhum usuário encontrado.'
+
+
 def sair():
     print 'Saindo...'
     nbio.close()
     sys.exit()
+
 
 def main():
     menu = """
--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import nbio
 print 'INIT:', nbio.init()
 print 'OPEN:', nbio.open()
 print 'GET_INFO:', nbio.get_info()
-print 'SET_INFO:', nbio.set_info(50,5000)
+print 'SET_INFO:', nbio.set_info(50, 5000)
 print 'GET_INFO:', nbio.get_info()
 text = nbio.enroll()
 print 'ENROLL:', text
